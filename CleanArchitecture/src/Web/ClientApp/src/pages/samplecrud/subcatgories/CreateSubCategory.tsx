72d3b334bd0401cb1d5dcc8390260dd75c8704e5
Rakshith Kumar M R <rakshith.kumar@pearlarc.com>
2024-06-20 14:23:10 +0530

    Removed Spinner from client-side.

--- CleanArchitecture/src/Web/ClientApp/src/pages/samplecrud/subcatgories/CreateSubCategory.tsx
+++ CleanArchitecture/src/Web/ClientApp/src/pages/samplecrud/subcatgories/CreateSubCategory.tsx
@@ -3,14 +3,12 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
 import SubCategoryService from "../shared/services/SubCategoryService";
 import SweetAlertService from "../../../services/SweetAlertService";
-import { Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Category_Regex, errorMessages, getMaxLengthErrorMessage, getRequiredErrorMessage, max_limit_Subcategory } from "../shared/constants/AppConstant";
 import CategoryService from "../shared/services/CategoryService";
 import { Category } from "../categories/models/Category";
 
 const CreateSubCategory = () => {
-    const [isShowLoader, setIsShowLoader] = useState(false);
     const navigate = useNavigate();
     const { register, handleSubmit, reset, formState: { errors }, } = useForm();
     const [categories, setCategories] = useState<Category[]>([]);
@@ -18,12 +16,9 @@ const CreateSubCategory = () => {
     //method to get categories through API
     const getAllCategories = async () => {
         try {
-            setIsShowLoader(true);
             const response = await CategoryService.getAllCategories();
             setCategories(response.data);
-            setIsShowLoader(false);
         } catch (error) {
-            setIsShowLoader(false);
             SweetAlertService.error(
                 "Error",
                 "Something went wrong while fetching categories"
@@ -35,10 +30,8 @@ const CreateSubCategory = () => {
     const onSubmit = async (data: any) => {
         const postData = { name: data.subcategory.trim(), categoryId: data.categoryId }; // Trim the category name
         try {
-            setIsShowLoader(true);
             const response = await SubCategoryService.postSubCategory(postData);
             if (response.data) {
-                setIsShowLoader(false);
                 reset();
                 await SweetAlertService.success(
                     "Success",
@@ -47,7 +40,6 @@ const CreateSubCategory = () => {
                 navigate("/subCategory");
             }
         } catch (error: any) {
-            setIsShowLoader(false);
             if (error?.response?.data?.errors?.Name[0]) {
                 SweetAlertService.error("Error", error.response.data.errors.Name[0]);
             } else {
@@ -62,90 +54,86 @@ const CreateSubCategory = () => {
 
     return (
         <>
-            {isShowLoader ? (<div className="loader-backdrop">
-                <Spinner animation="border" variant="primary" />
-            </div>) : (<>
-                <div className="panel-body">
-                    <div className="header-panel d-flex justify-content-between">
-                        <h1>Create SubCategory</h1>
-                        <Link className="fs-5 fw-bold text-blue-800 align-self-center"
-                            type="button"
-                            to="/subCategory">
-                            Back
-                        </Link>
-                    </div>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className="mb-15px">
-                            <div className="card border-2 p-4">
-                                <div className="row mt-3">
-                                    <div className="col-md-6">
-                                        <label className="mb-2">
-                                            <b>Create SubCategory</b>
-                                        </label>
-                                        <input
-                                            type="text"
-                                            id="description"
-                                            className="form-control"
-                                                {...register("subcategory", {
-                                                required: getRequiredErrorMessage("Subcategory"),
-                                                pattern: {
-                                                    value: Category_Regex,
-                                                    message: errorMessages?.pattern,
-                                                },
-                                                maxLength: {
-                                                    value: max_limit_Subcategory,
-                                                    message: getMaxLengthErrorMessage("Subcategory", max_limit_Subcategory),
-                                                },
-                                            })}
-                                        />
-                                            {errors.subcategory && (
-                                            <p className="text-danger">
-                                                    {errors.subcategory.message?.toString()}
-                                            </p>
-                                        )}
-                                    </div>
-                                    </div>
-                                    <div className="row mt-3">
-                                        <div className="col-md-6">
-                                            <label className="mb-2">
-                                                <b>Choose Category</b>
-                                            </label>
-                                            <select
-                                                className="form-select"
-                                                {...register("categoryId", {
-                                                    required: getRequiredErrorMessage("Category"),
-                                                })}
-                                            >
-                                                <option value="" className="d-none">
-                                                    Choose Category
-                                                </option>
-                                                {categories.map((item) => (
-                                                    <option key={item.id} value={item.id}>
-                                                        {item.name}
-                                                    </option>
-                                                ))}
-                                            </select>
-                                            {errors.categoryId && (
-                                                <p className="text-danger">
-                                                    {errors.categoryId.message?.toString()}
-                                                </p>
-                                            )}
-                                        </div>
-                                    </div>
-                                <div className="mt-3">
-                                    <button className="fs-15px fw-bold btn btn-dark bg-blue-800">
-                                        Create SubCategory
-                                    </button>
-                                    <button className="fw-bold btn btn-secondary mx-2"
-                                        onClick={() => navigate("/subCategory")}>
-                                        Discard
-                                    </button>
+            <div className="panel-body">
+                <div className="header-panel d-flex justify-content-between">
+                    <h1>Create SubCategory</h1>
+                    <Link className="fs-5 fw-bold text-blue-800 align-self-center"
+                        type="button"
+                        to="/subCategory">
+                        Back
+                    </Link>
+                </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-15px">
+                        <div className="card border-2 p-4">
+                            <div className="row mt-3">
+                                <div className="col-md-6">
+                                    <label className="mb-2">
+                                        <b>Create SubCategory</b>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="description"
+                                        className="form-control"
+                                        {...register("subcategory", {
+                                            required: getRequiredErrorMessage("Subcategory"),
+                                            pattern: {
+                                                value: Category_Regex,
+                                                message: errorMessages?.pattern,
+                                            },
+                                            maxLength: {
+                                                value: max_limit_Subcategory,
+                                                message: getMaxLengthErrorMessage("Subcategory", max_limit_Subcategory),
+                                            },
+                                        })}
+                                    />
+                                    {errors.subcategory && (
+                                        <p className="text-danger">
+                                            {errors.subcategory.message?.toString()}
+                                        </p>
+                                    )}
                                 </div>
                             </div>
+                            <div className="row mt-3">
+                                <div className="col-md-6">
+                                    <label className="mb-2">
+                                        <b>Choose Category</b>
+                                    </label>
+                                    <select
+                                        className="form-select"
+                                        {...register("categoryId", {
+                                            required: getRequiredErrorMessage("Category"),
+                                        })}
+                                    >
+                                        <option value="" className="d-none">
+                                            Choose Category
+                                        </option>
+                                        {categories.map((item) => (
+                                            <option key={item.id} value={item.id}>
+                                                {item.name}
+                                            </option>
+                                        ))}
+                                    </select>
+                                    {errors.categoryId && (
+                                        <p className="text-danger">
+                                            {errors.categoryId.message?.toString()}
+                                        </p>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="mt-3">
+                                <button className="fs-15px fw-bold btn btn-dark bg-blue-800">
+                                    Create SubCategory
+                                </button>
+                                <button className="fw-bold btn btn-secondary mx-2"
+                                    onClick={() => navigate("/subCategory")}>
+                                    Discard
+                                </button>
+                            </div>
                         </div>
-                    </form>
-                </div>
-            </>)}
+                    </div>
+                </form>
+            </div>
         </>
     );
 };
--- CleanArchitecture/src/Web/ClientApp/src/pages/samplecrud/subcatgories/SubCategoryList.tsx
+++ CleanArchitecture/src/Web/ClientApp/src/pages/samplecrud/subcatgories/SubCategoryList.tsx
@@ -2,24 +2,19 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import SweetAlertService from "../../../services/SweetAlertService";
 import { DeletePopupConfirmationMessage } from "../shared/constants/AppConstant";
-import { Spinner } from "react-bootstrap";
 import { SubCategory } from "./models/SubCategory";
 import SubCategoryService from "../shared/services/SubCategoryService";
 
 const SubCategoryList = () => {
     const navigate = useNavigate();
     const [subCategories, setSubCategories] = useState<SubCategory[]>([]);
-    const [isShowLoader, setIsShowLoader] = useState(false);
 
     //method to get categories through API
     const getAllSubCategories = async () => {
         try {
-            setIsShowLoader(true);
             const response = await SubCategoryService.getAllSubCategories();
             setSubCategories(response.data);
-            setIsShowLoader(false);
         } catch (error) {
-            setIsShowLoader(false);
             SweetAlertService.error(
                 "Error",
                 "Something went wrong while fetching sub-categories"
@@ -50,64 +45,60 @@ const SubCategoryList = () => {
 
     return (
         <>
-            {isShowLoader ? (<div className="loader-backdrop">
-                <Spinner animation="border" variant="primary" />
-            </div>) : (<>
-                <div className="row p-1 col-md-12">
-                    <div className="col-md-8">
-                        <h1>Sub Categories</h1>
-                    </div>
-                    <div className="col-md-4 d-flex justify-content-end">
-                        <button
-                            className="fw-bold btn btn-dark bg-blue-800 me-3"
-                            onClick={() => navigate("/createSubCategory")}
-                        >
-                            Create SubCategory
-                        </button>
-                    </div>
+            <div className="row p-1 col-md-12">
+                <div className="col-md-8">
+                    <h1>Sub Categories</h1>
                 </div>
-                <div className="panel table-responsive content mt-2">
-                        {
-                            subCategories.length > 0 ? (<>
-                            <table className="table table-bordered table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">ID</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Category</th>
-                                        <th scope="col"></th>
+                <div className="col-md-4 d-flex justify-content-end">
+                    <button
+                        className="fw-bold btn btn-dark bg-blue-800 me-3"
+                        onClick={() => navigate("/createSubCategory")}
+                    >
+                        Create SubCategory
+                    </button>
+                </div>
+            </div>
+            <div className="panel table-responsive content mt-2">
+                {
+                    subCategories.length > 0 ? (<>
+                        <table className="table table-bordered table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Category</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {subCategories.map((subCategory, index) => (
+                                    <tr key={subCategory.id}>
+                                        <th scope="row">{index + 1}</th>
+                                        <td>{subCategory?.name}</td>
+                                        <td>{subCategory?.categories?.name}</td>
+                                        <td>
+                                            <button
+                                                className="fw-bold btn btn-dark bg-blue-800 me-3"
+                                                onClick={() => navigate(`/updatesubcategory/${btoa(subCategory?.id.toString())}`)}
+                                            >
+                                                Edit
+                                            </button>
+                                            <button
+                                                className="fw-bold btn btn-secondary"
+                                                onClick={() => onDeleteClick(subCategory.id)}
+                                            >
+                                                Delete
+                                            </button>
+                                        </td>
                                     </tr>
-                                </thead>
-                                    <tbody>
-                                        {subCategories.map((subCategory, index) => (
-                                            <tr key={subCategory.id}>
-                                            <th scope="row">{index + 1}</th>
-                                                <td>{subCategory?.name}</td>
-                                                <td>{subCategory?.categories?.name}</td>
-                                            <td>
-                                                <button
-                                                        className="fw-bold btn btn-dark bg-blue-800 me-3"
-                                                        onClick={() => navigate(`/updatesubcategory/${btoa(subCategory?.id.toString())}`)}
-                                                >
-                                                    Edit
-                                                </button>
-                                                <button
-                                                    className="fw-bold btn btn-secondary"
-                                                        onClick={() => onDeleteClick(subCategory.id)}
-                                                >
-                                                    Delete
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </>) : (<>
-                            <h4>No sub-categories found.</h4>
-                        </>)
-                    }
-                </div>
-            </>)}
+                                ))}
+                            </tbody>
+                        </table>
+                    </>) : (<>
+                        <h4>No sub-categories found.</h4>
+                    </>)
+                }
+            </div>
         </>
     );
 }
